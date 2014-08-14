e137bf698861654c66a48d8205a0adaf25556b3e
Rushaine McBean <rushaine.mcbean@gmail.com>
2014-08-13 20:13:26 -0400

    Create a verisioned api endpoints for notes

---
+++ app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,51 @@
+module Api
+  module V1
+    class NotesController < ApplicationController
+      before_action :set_note, only: [:show, :update, :destroy]
+
+      def index
+        @notes = Note.all
+        render json: @notes
+      end
+
+      def show
+        render json: @note
+      end
+
+      def create
+        @note = Note.new(note_params)
+
+        if @note.save
+          render json: @note, status: :created, location: @note
+        else
+          render json: @note.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+
+        if @note.update(note_params)
+          head :no_content
+        else
+          render json: @note.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @note.destroy
+
+        head :no_content
+      end
+
+      private 
+
+      def set_note
+        @note = Note.find(params[:id])
+      end
+
+      def note_params
+        params.requrie(:note).permit(:title, :body, :category_id) if params[:note]
+      end
+    end
+  end
+end

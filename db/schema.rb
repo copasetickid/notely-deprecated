03ef07ad39ac3593cab98d8189b198e7ea29ae30
Rushaine McBean <rushaine.mcbean@gmail.com>
2014-08-13 20:21:28 -0400

    Create a category model and an association to notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813233916) do
+ActiveRecord::Schema.define(version: 20140814001726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "notes", force: true do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end

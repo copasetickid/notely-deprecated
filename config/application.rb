386bd8aae156dc62629f48f6be730d7db23c6438
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-01-14 18:45:39 -0500

    Setup rspec

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module Notely
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |generate|
+      generate.test_framework :rspec
+    end
   end
 end
